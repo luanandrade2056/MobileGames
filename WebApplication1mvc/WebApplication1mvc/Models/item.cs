94a87a0fc74802245b24cf758409c7afb7a18477
luanandrade2056 <luanandrade2056@gmail.com>
2017-10-16 17:18:37 -0200

    versao do projeto

---
+++ WebApplication1mvc/WebApplication1mvc/Models/item.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebApplication1mvc.Models
+{
+    public class item
+    {
+        public int ItemID { get; set; }
+        public string Nome { get; set; }
+        public string Descriçao { get; set; }
+        public int Preco { get; set; }
+    }
+}
