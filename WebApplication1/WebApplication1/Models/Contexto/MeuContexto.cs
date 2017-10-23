1fcf6bcf6115700d15dbc37ff134e98a83e33c61
luanandrade2056 <luanandrade2056@gmail.com>
2017-10-23 15:34:24 -0200

    criando o contexto 2

---
+++ WebApplication1/WebApplication1/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Data.Entity;
+
+namespace WebApplication1.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+    }
+}
